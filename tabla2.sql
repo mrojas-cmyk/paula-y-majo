79fa467a37fad8752355fe4b8975886c47bfe717
mrojas-cmyk <mrojas@sagradoscorazonesmsq.edu.co>
2026-05-15 11:28:30 -0500

    Add files via upload

---
+++ tabla2.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "mytable" (
+  "CREATE TABLE Autores (" text
+);
+
+INSERT INTO "mytable" ("CREATE TABLE Autores (")
+VALUES
+('AutorID INT PRIMARY KEY'),
+('Nombre VARCHAR(50)'),
+('Apellido VARCHAR(50)'),
+('Nacionalidad VARCHAR(50)'),
+(');');
